356faba7bba2e9726566a92fc54edb7ce715a940
xorms571 <xorms6865@naver.com>
2024-11-13 07:46:34 +0900

    ffinal3

--- src/app/utils/api.ts
+++ src/app/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://bigsback-production.up.railway.app/",
+  baseURL: "https://bigsback-production.up.railway.app",
   withCredentials: true,
 });
 
